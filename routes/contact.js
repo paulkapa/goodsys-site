113e313a21be39bd94e5989c08ac30c62ac5e47c
paulkapa <paulkapa@yahoo.com>
2020-07-15 22:55:49 +0300

    FIx: URL resolution

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var contactController = require('../controllers/contact.js');
 /* GET contact page. */
-router.get('../en/contact', contactController.getContactEN);
-router.get('../ro/contact', contactController.getContactRO);
+router.get('/en/contact', contactController.getContactEN);
+router.get('/ro/contact', contactController.getContactRO);
 
 module.exports = router;
--- routes/maintenance.js
+++ routes/maintenance.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var maintenanceController = require('../controllers/maintenance.js');
 /* GET maintenance page. */
-router.get('../en/maintenance', maintenanceController.getMaintenanceEN);
-router.get('../ro/maintenance', maintenanceController.getMaintenanceRO);
+router.get('/en/maintenance', maintenanceController.getMaintenanceEN);
+router.get('/ro/maintenance', maintenanceController.getMaintenanceRO);
 
 module.exports = router;
