7398a97a7ddebd08284817c919683db27393b977
paulkapa <paulkapa@yahoo.com>
2020-07-15 23:02:50 +0300

    Fix: directory include paths

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,3 @@
-exports.getIndex = (req, res, next) => {
-    res.render('index', { title: 'GoodSys', lang : 'RO' });
-}
-
 exports.getIndexEN = (req, res, next) => {
     res.render('index', { title: 'GoodSys', lang : 'EN' });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index.js');
 /* GET home page. */
-router.get('goodsys.azurewebsites.net/', indexController.getIndex);
+router.get('goodsys.azurewebsites.net', indexController.getIndexRO);
 router.get('goodsys.azurewebsites.net/en', indexController.getIndexEN);
 router.get('goodsys.azurewebsites.net/ro', indexController.getIndexRO);
 
