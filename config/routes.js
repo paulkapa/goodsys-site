a299210cbdd73f003c23e4b3f2472633809808e3
paulkapa <paulkapa@yahoo.com>
2020-07-15 17:22:11 +0300

    Changed routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 var indexRouter = require('../routes/index');
+var contactRouter = require('../routes/contact');
+var copyrightRouter = require('../routes/copyright');
+var maintenanceRouter = require('../routes/maintenance');
 
 module.exports = (app) => {
     app.use('/', indexRouter);
+    app.use('/contact', contactRouter);
+    app.use('/copyright', copyrightRouter);
+    app.use('/maintenance', maintenanceRouter);
     // !-- Do not remove this line --! //
 };
