7479a1f950e699ae706f33565cf5f110d8cae684
paulkapa <paulkapa@yahoo.com>
2020-07-21 10:19:12 +0300

    Lots of changes - added subscriptions views/ctrl's

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,13 @@ var indexRouter = require('../routes/index');
 var contactRouter = require('../routes/contact');
 var copyrightRouter = require('../routes/copyright');
 var maintenanceRouter = require('../routes/maintenance');
+var subscriptionsRouter = require('../routes/subscriptions');
 
 module.exports = (app) => {
     app.use('/', indexRouter);
     app.use('/', contactRouter);
     app.use('/', copyrightRouter);
     app.use('/', maintenanceRouter);
+    app.use('/', subscriptionsRouter);
     // !-- Do not remove this line --! //
 };
